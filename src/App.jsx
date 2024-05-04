13baadc85c9ab9f005d78496a827fb9be3de26cc
Yana Lepska <lepskayana1624@gmail.com>
2024-05-05 00:11:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import Layout from "./components/Layout/Layout";
-import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import { lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { refreshUser } from "./redux/auth/operations";
 import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Loader from "./components/Loader/Loader";
+import Layout from "./components/Layout/Layout";
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
+const RegistrationPage = lazy(() => import("./pages/RegistrationPage/RegistrationPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+
 
 const App = () => {
   const dispatch = useDispatch();
@@ -18,6 +21,7 @@ const App = () => {
   return (
     <>
       <Layout>
+        <Suspense fallback={<div><Loader /></div>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route
@@ -38,7 +42,8 @@ const App = () => {
           />
           <Route path="/contacts" element={<PrivateRoute><ContactsPage /></PrivateRoute>} />
           <Route path="*" element="Not found" />
-        </Routes>
+          </Routes>
+          </Suspense>
       </Layout>
     </>
   );
